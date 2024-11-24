d6db04942e07b6f77dab7590e750e64ad98d329d
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2024-11-25 01:43:34 +0600

    read me file added

--- src/Modules/product/product.service.ts
+++ src/Modules/product/product.service.ts
@@ -2,6 +2,7 @@ import { ProductModel } from '../product.model';
 import { Product } from './product.interface';
 
 const createProductIntoDB = async (product: Product) => {
+  
   const result = await ProductModel.create(product);
   return result;
 };
